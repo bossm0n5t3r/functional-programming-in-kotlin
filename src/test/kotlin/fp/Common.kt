e32b3099071609c33beed5339d2a1510fb3f997a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-10-06 18:32:23 +0900

    feat: Add Common

---
+++ src/test/kotlin/fp/Common.kt
@@ -0,0 +1,33 @@
+package fp
+
+fun List<Int>.head() = first()
+fun List<Int>.tail() = drop(1)
+
+fun String.head() = first()
+fun String.tail() = drop(1)
+
+fun <T> Sequence<T>.head() = first()
+fun <T> Sequence<T>.tail() = drop(1)
+
+fun <T> Set<T>.head() = first()
+fun <T> Set<T>.tail() = drop(1).toSet()
+
+fun <T> Collection<T>.head() = first()
+fun <T> Collection<T>.tail() = drop(1)
+
+operator fun <T> Sequence<T>.plus(other: () -> Sequence<T>) = object : Sequence<T> {
+    private val thisIterator: Iterator<T> by lazy { this@plus.iterator() }
+    private val otherIterator: Iterator<T> by lazy { other().iterator() }
+    override fun iterator() = object : Iterator<T> {
+        override fun next(): T =
+            if (thisIterator.hasNext()) {
+                thisIterator.next()
+            } else if (otherIterator.hasNext()) {
+                otherIterator.next()
+            } else {
+                throw NoSuchElementException()
+            }
+
+        override fun hasNext(): Boolean = thisIterator.hasNext() || otherIterator.hasNext()
+    }
+}
