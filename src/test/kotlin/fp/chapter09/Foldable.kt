04b50108459b01f432cfa96c1de617380103603a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-12-05 04:15:10 +0900

    feat: Add Foldable

---
+++ src/test/kotlin/fp/chapter09/Foldable.kt
@@ -0,0 +1,6 @@
+package fp.chapter09
+
+interface Foldable<out A> {
+    fun <B> foldLeft(acc: B, f: (B, A) -> B): B
+    fun <B> foldMap(f: (A) -> B, m: Monoid<B>): B = foldLeft(m.mempty()) { b, a -> m.mappend(b, f(a)) }
+}
